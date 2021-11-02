8edfc78341b89e82386634425d6fcf660d2933e2
denisaqosja <denisaqosja97@gmail.com>
2021-11-02 13:21:07 +0100

    Cleaning old code

--- Multi-Layer Perceptron/src/data.py
+++ Multi-Layer Perceptron/src/data.py
@@ -36,25 +36,3 @@ def load_test_dataset():
     data = datasets.CIFAR10("../data/", train=False, download=True, transform=transforms.ToTensor())
     trainLoader = DataLoader(data, batch_size=BS, shuffle=False)
     return trainLoader
-
-
-"""
-rom torchvision import datasets
-from torchvision.transforms import transforms
-from torch.utils.data import DataLoader
-
-import numpy as np
-
-BS = 128
-
-def load_train_dataset():
-    data = datasets.CIFAR10("../data/", train=True, download=True, transform=transforms.ToTensor())
-    trainLoader = DataLoader(data, batch_size=BS, shuffle=True)
-
-    return trainLoader
-
-def load_test_dataset():
-    data = datasets.CIFAR10("../data/", train=False, download=True, transform=transforms.ToTensor())
-    trainLoader = DataLoader(data, batch_size=BS, shuffle=False)
-    return trainLoader
-"""
