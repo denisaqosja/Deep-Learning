379ed4de41993992a82c0e6a863e59e48e6e7cd3
denisaqosja <denisaqosja97@gmail.com>
2021-11-03 16:17:48 +0100

    Fixing small errors

--- ConvNet/src/optuna_study.py
+++ ConvNet/src/optuna_study.py
@@ -2,30 +2,31 @@ import pickle
 import optuna
 
 from model import CNN
-from train import Trainer
+from trainer import Trainer
 from utils import set_optimizer
 
 def objective(trial):
 
     model = CNN()
-    optimizer = trial.suggest_categorical("SGD", "SGD_momentum", "RMSprop", "Adam")
-    learning_rate = trial.suggest_loguniform(1e-5, 5e-2)
-    regularizer = trial.suggest_categorical("L1", "L2", "Elastic", "None")
+    optimizer = trial.suggest_categorical("optimizer", ["SGD", "SGD_momentum", "RMSprop", "Adam"])
+    learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1e-2)
+    regularizer = trial.suggest_categorical("regularizer", ["L1", "L2", "Elastic", "None"])
 
     trainer = Trainer()
     trainer.setup_model()
     trainer.optimizer = set_optimizer(optimizer, model.parameters(), learning_rate)
     trainer.lr = learning_rate
-    trainer.regularizer = regularizer
+    trainer.regularizerName = regularizer
+    trainer.train_model()
 
-    accuracy = trainer.eval_acc
+    accuracy = trainer.test_acc
 
     return accuracy
 
 
 if __name__=="__main__":
     study = optuna.create_study(direction="maximize")
-    study.optimize(objective)
+    study.optimize(objective, n_trials = 2)
 
     with open("optuna_study.pkl", "wb") as file:
-        pickle.dump(file)
+        pickle.dump(study, file)
